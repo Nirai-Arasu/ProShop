7b2140c0f565f92192849a1c550c69bd4e907000
Nirai Arasu <niraiarasu.listertechnologies.com>
2023-06-07 23:08:04 +0530

    header and footer components done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import Header from './components/Header';
+import { Container } from 'react-bootstrap';
+
+import Footer from './components/Footer';
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <main className="py-3">
+      <Header />
+      <Container></Container>
+      <Footer />
+    </main>
+  );
+};
 
-export default App
+export default App;
