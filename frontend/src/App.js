fa493284534754b975e93a3726a5c474528e36c9
Nirai Arasu <niraiarasu.listertechnologies.com>
2023-06-12 21:58:28 +0530

    frontend-functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import Header from './components/Header';
 import { Container } from 'react-bootstrap';
-
+import { Outlet } from 'react-router-dom';
 import Footer from './components/Footer';
 
 const App = () => {
   return (
     <main className="py-3">
       <Header />
-      <Container></Container>
+      <Container>
+        <Outlet />
+      </Container>
       <Footer />
     </main>
   );
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from 'react-router-dom';
 import './assets/styles/bootstrap.custom.css';
 import './assets/styles/index.css';
 import App from './App';
-
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element=<App />>
+      <Route index element=<HomeScreen /> />
+
+      <Route path="product/:id" element={<ProductScreen />} />
+    </Route>
+  )
+);
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
